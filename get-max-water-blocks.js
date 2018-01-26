37d3672f34ed340c49ab582b273dbf90bb4012f3
AJ Pawlicki <aloysiuspaw@gmail.com>
2018-01-25 22:53:20 -0800

    Create main function and helper functions

---
+++ get-max-water-blocks.js
@@ -0,0 +1,61 @@
+const getMaxWaterBlocks = heights => {
+  if (!heights || heights.length < 3) throw new Error('Need an input array length of at least 3.');
+  
+  let leftWall = 0;
+  let rightWall = 0;
+  let maxLeftWall = 0;
+  let maxWaterBlocks = 0;
+  let currentWaterBlocks = 0
+
+  const waterBlocks = getWaterBlocks(heights);
+
+  for (let i = 0; i < waterBlocks.length; i++) {
+    if (waterBlocks[i] === 0) {
+      if (currentWaterBlocks > maxWaterBlocks) {
+        maxWaterBlocks = currentWaterBlocks;
+        rightWall = i + 1;
+        maxLeftWall = leftWall;
+      }
+
+      currentWaterBlocks = 0;
+      leftWall = i + 1;
+    } else {
+      currentWaterBlocks += waterBlocks[i];
+    }
+  }
+
+  return [ maxLeftWall, rightWall, maxWaterBlocks ];
+};
+
+const getWaterBlocks = heights => {
+  const waterBlocks = [];
+  let leftMaxHeight = 0;
+
+  for (let i = 0; i < heights.length; i++) {
+    const minOfHeights = Math.min(leftMaxHeight, getRightMaxHeight(heights, i + 1));
+    
+    waterBlocks[i] = Math.max(0, minOfHeights - heights[i]);
+
+    leftMaxHeight = Math.max(leftMaxHeight, heights[i]);
+  }
+
+  return waterBlocks;
+};
+
+const getRightMaxHeight = (heights, start) => {
+  if (start >= heights.length) return 0;
+
+  let rightMaxHeight = heights[start];
+  
+  for (let i = start + 1; i < heights.length; i++) {
+    rightMaxHeight = Math.max(heights[i], rightMaxHeight);
+  }
+
+  return rightMaxHeight;
+};
+
+module.exports = {
+  getRightMaxHeight,
+  getMaxWaterBlocks,
+  getWaterBlocks
+};
